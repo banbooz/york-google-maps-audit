608d47c85a74bd1fd8d3d9c68538efe47658cf40
banbooz <banboozplays@gmail.com>
2026-06-01 21:26:01 +0100

    Add Firebase config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase setup file.
+// Replace these values with your Firebase web app config from Firebase Console.
+// This config is allowed to be public, but your Firestore rules must protect the data.
+window.firebaseConfig = {
+  apiKey: "PASTE_API_KEY_HERE",
+  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
